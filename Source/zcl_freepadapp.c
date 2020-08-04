d0e1bf133a0172dd431f4eefd67a23c6328deb72
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2020-08-05 00:14:43 +0500

    Update timer REPORT_EVT and ReportBattery
    Update timer FREEPADAPP_REPORT_EVT and zclFreePadApp_ReportBattery

--- Source/zcl_freepadapp.c
+++ Source/zcl_freepadapp.c
@@ -330,8 +330,12 @@ static void zclFreePadApp_HandleKeys(byte shift, byte keyCode) {
     prevKeyCode = keyCode;
 
     if (keyCode == HAL_KEY_CODE_RELEASE_KEY) {
-        zclFactoryResetter_HandleKeys(0x40, keyCode);
+        zclFactoryResetter_HandleKeys(0x40, keyCode);        
+#if (!defined PM3)
         osal_start_timerEx(zclFreePadApp_TaskID, FREEPADAPP_REPORT_EVT, 20 * 1000);
+#elif (defined PM3)
+        osal_start_timerEx(zclFreePadApp_TaskID, FREEPADAPP_REPORT_EVT, 3 * 1000);
+#endif         
 #ifdef FREEPAD_ENABLE_TL
         osal_stop_timerEx(zclFreePadApp_TaskID, FREEPADAPP_TL_START_EVT);
 #endif
@@ -397,7 +401,26 @@ static void zclFreePadApp_ReportBattery(void) {
 
     LREP("Battery voltageZCL=%d prc=%d voltage=%d\r\n", zclFreePadApp_BatteryVoltage, zclFreePadApp_BatteryPercentageRemainig,
          getBatteryVoltage());
+#if (!defined PM3)    
     bdb_RepChangedAttrValue(1, ZCL_CLUSTER_ID_GEN_POWER_CFG, ATTRID_POWER_CFG_BATTERY_PERCENTAGE_REMAINING);
+#elif (defined PM3) 
+  const uint8 NUM_ATTRIBUTES = 2;
+  zclReportCmd_t *pReportCmd;
+  pReportCmd = osal_mem_alloc(sizeof(zclReportCmd_t) + (NUM_ATTRIBUTES * sizeof(zclReport_t)));
+  if (pReportCmd != NULL) {
+    pReportCmd->numAttr = NUM_ATTRIBUTES;
+    
+    pReportCmd->attrList[0].attrID = ATTRID_POWER_CFG_BATTERY_VOLTAGE;
+    pReportCmd->attrList[0].dataType = ZCL_DATATYPE_UINT8;
+    pReportCmd->attrList[0].attrData = (void *)(&zclFreePadApp_BatteryVoltage); 
+    
+    pReportCmd->attrList[1].attrID = ATTRID_POWER_CFG_BATTERY_PERCENTAGE_REMAINING;
+    pReportCmd->attrList[1].dataType = ZCL_DATATYPE_UINT8;
+    pReportCmd->attrList[1].attrData = (void *)(&zclFreePadApp_BatteryPercentageRemainig);
+    
+    zcl_SendReportCmd(1, &inderect_DstAddr, ZCL_CLUSTER_ID_GEN_POWER_CFG, pReportCmd, ZCL_FRAME_CLIENT_SERVER_DIR, TRUE, bdb_getZCLFrameCounter());
+  }
+  osal_mem_free(pReportCmd);
 }
 
 static void zclFreePadApp_RestoreAttributesFromNV(void) {
